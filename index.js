544d45b2967a3b859b2154c52d98c488b30f73bd
Mustafa Farah <mustafa.farah@outlook.com>
2018-02-24 14:08:43 -0600

    done

---
+++ index.js
@@ -0,0 +1,24 @@
+// 'use strict';'
+
+const mustafa = {
+   name: "mustafa",
+   age:27,
+   happy: true,
+   hairColor:"brown"
+} ;
+
+
+const mustafa = [name, happy, age, hairColor, ['kids', 'dogs', 4]];
+
+for (let index = 0; index < mustafa.length; index++) {  
+console.log(mustafa[index]);
+}
+
+// console.log(typeof hairColor, hairColor);
+
+// console.log(typeof name, name);
+// console.log(typeof happy, happy);
+// happy = false;
+// console.log(typeof happy, happy);
+// console.log(typeof age, age);
+
